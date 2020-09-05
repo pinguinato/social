2cc54ed1ec0b74ef23ed1b2f76f67ba208c08248
roberto <gianottoroberto@gmail.com>
2020-09-05 22:59:48 +0200

    reset dei dati di sessione, storing del record a db, assegnamento immagine profilo

--- demo/register.php
+++ demo/register.php
@@ -1,7 +1,7 @@
 <?php 
 session_start();
 
-# parametri di connessione al mio db locale
+// parametri di connessione al mio db locale
 $host = "mysql57";
 $user = "root";
 $password = "root";
@@ -9,6 +9,7 @@ $database ="social";
 
 $conn = mysqli_connect($host, $user, $password, $database);
 
+// connection check
 if (mysqli_connect_errno()) {
     echo "Failed to connect " . mysqli_connect_errno();
 }
@@ -22,6 +23,7 @@ define("EMAILS_NOT_MATCH", "Emails don't match<br>");
 define("PASSWORDS_NOT_MATCH", "Your passwords do not match<br>");
 define("PASSWORD_WRONG_FORMAT", "Your password can only contain english characters or numbers<br>");
 define("PASSWORD_WRONG_SIZE", "Your password must be beetween 5 and 30 characters<br>");
+define("REGISTRATION_OK", "<span style='color: #14C800;'>You're all set! Go ahead and login!</span><br>");
 
 #variabili del form di registrazione
 $fname = ""; // first name
@@ -50,12 +52,12 @@ $_SESSION['reg_lname'] = $lname; // store last name in the session variable
 // email
 $em = strip_tags($_POST['reg_email']); // remove html tags
 $em = str_replace(' ', '', $em); // remove spaces
-$em = ucfirst(strtolower($em)); // put a capital letter and all lowercase
+$em = strtolower($em); // put a capital letter and all lowercase
 $_SESSION['reg_email'] = $em; // store email in the session variable
 //email 2
 $em2 = strip_tags($_POST['reg_email2']); // remove html tags
 $em2 = str_replace(' ', '', $em2); // remove spaces
-$em2 = ucfirst(strtolower($em2)); // put a capital letter and all lowercase
+$em2 = strtolower($em2); // put a capital letter and all lowercase
 $_SESSION['reg_email2'] = $em2; // store email 2 in the session variable
 // password & password2
 $password = strip_tags($_POST['reg_password']);
@@ -105,7 +107,47 @@ $date = date("Y-m-d"); // register current date
     if(strlen($password) > 30 || strlen($password) < 5) {
         array_push($error_array, PASSWORD_WRONG_SIZE);
     }
+
+    // if no errors in the form
+    if(empty($error_array)) {
+        $password = md5($password); // encrypt password before sending to database
+        
+        // generate user name by concatenating first and last name
+        $username = strtolower($fname . "_" . $lname);
+        $check_username_query = mysqli_query($conn, "SELECT username FROM users WHERE username='$username'");
+        $i = 0;
+        // if username exists
+        while(mysqli_num_rows($check_username_query) != 0) {
+            $i++;
+            $username = $username . "_" . $i;
+            $check_username_query = mysqli_query($conn, "SELECT username FROM users WHERE username='$username'");
+        }
+    }
+
+    // profile picture assignment
+    $rand = rand(1, 2);
+    if($rand == 1) {
+        $profile_pic = "assets/images/profile_pics/defaults/head_deep_blue.png";
+    } else if ($rand == 2) {
+        $profile_pic = "assets/images/profile_pics/defaults/head_green_sea.png";
+    }
+    
+
+    // TODO: c'è un problema quando inserisce ugualmente un record privo di username, devo risolvere
+    // insert data into database
+    $query = mysqli_query($conn, "INSERT INTO users VALUES (NULL, '$fname', '$lname', '$username', '$em', '$password', '$date', '$profile_pic', '0', '0', 'no', '')");
+    array_push($error_array, REGISTRATION_OK);
+    // TODO: qui si potrebbe mettere un qualcosa nel caso fallisce la query di inserimento a db tipo un KO
+
+    // clear session variables
+    $_SESSION['reg_fname'] = "";
+    $_SESSION['reg_lname'] = "";
+    $_SESSION['reg_email'] = "";
+    $_SESSION['reg_email2'] = "";
+
 }
+
+
 ?>
 
 <!DOCTYPE html>
@@ -166,8 +208,21 @@ $date = date("Y-m-d"); // register current date
         <?php if (in_array(PASSWORDS_NOT_MATCH, $error_array)) { echo PASSWORDS_NOT_MATCH; } ?>
 
         <input type="submit" name="register_button" value="Register">
-        
+        <br>
+        <?php if (in_array(REGISTRATION_OK, $error_array)) { echo REGISTRATION_OK; } ?>
+
     </form>
 </body>
 
 </html>
+
+
+
+<?php 
+
+
+// debug
+
+echo "<h2>Debug</h2>";
+
+var_dump($fname, $lname, $em, $em2, $password, $password2, $date, $username); 
