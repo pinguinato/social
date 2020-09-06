2f0759cbb5152af8cfbf3467a2ac5a5ad713696d
roberto <gianottoroberto@gmail.com>
2020-09-06 11:14:13 +0200

    stilizzazione della schermata di login e registrazione inizio

--- demo/register.php
+++ demo/register.php
@@ -17,83 +17,96 @@ require 'includes/form_handlers/login_handler.php';
 
 <body>
 
+<div class="wrapper"> <!-- start of class wrapper -->
+    
+        <div class="login_box">
+    
+            <div class = "login_header">
+                <h1>Swirlfeed</h1>
+                Login or signup below!
+            </div>
+    
     <!-- start login form -->
-    <form action="register.php" method="POST">
-        <input type="email" name="log_email" placeholder="Email Address" value="<?php
-        if (isset($_SESSION['log_email'])) {
-            echo $_SESSION['log_email'];
-        }
-        ?>" required>
-        <br>
-        <input type="password" name="log_password" placeholder="Password" value="<?php
-        if (isset($_SESSION['log_password'])) {
-            echo $_SESSION['log_password'];
-        }
-        ?>" required>
-        <br>
-        <input type="submit" name="login_button" value="Login">
-        <br>
+        <form action="register.php" method="POST">
+            <input type="email" name="log_email" placeholder="Email Address" value="<?php
+            if (isset($_SESSION['log_email'])) {
+                echo $_SESSION['log_email'];
+            }
+            ?>" required>
+            <br>
+            <input type="password" name="log_password" placeholder="Password" value="<?php
+            if (isset($_SESSION['log_password'])) {
+                echo $_SESSION['log_password'];
+            }
+            ?>" required>
+            <br>
+            <input type="submit" name="login_button" value="Login">
+            <br>
 
-        <?php if (in_array(LOGIN_KO, $error_array)) { echo LOGIN_KO; } ?>
+            <?php if (in_array(LOGIN_KO, $error_array)) { echo LOGIN_KO; } ?>
 
-    </form>
+        </form>
     <!-- end login form -->
 
     <!-- start Registration form -->
-    <form action="register.php" method="POST">
-        <input type="text" name="reg_fname" placeholder="First Name" value="<?php
-        if (isset($_SESSION['reg_fname'])) {
-            echo $_SESSION['reg_fname'];
-        }
-        ?>" required>
-        <br>
-        
-        <?php if (in_array(WRONG_FIRST_NAME, $error_array)) {
-            echo WRONG_FIRST_NAME;
-        } ?>
+        <form action="register.php" method="POST">
+            <input type="text" name="reg_fname" placeholder="First Name" value="<?php
+            if (isset($_SESSION['reg_fname'])) {
+                echo $_SESSION['reg_fname'];
+            }
+            ?>" required>
+            <br>
 
-        <input type="text" name="reg_lname" placeholder="Last Name" value="<?php 
-        if (isset($_SESSION['reg_lname'])) {
-            echo $_SESSION['reg_lname'];
-        }
-        ?>" required>
-        <br>
+            <?php if (in_array(WRONG_FIRST_NAME, $error_array)) {
+                echo WRONG_FIRST_NAME;
+            } ?>
 
-        <?php if (in_array(WRONG_LAST_NAME, $error_array)) {
-            echo WRONG_LAST_NAME;
-        } ?>
+            <input type="text" name="reg_lname" placeholder="Last Name" value="<?php 
+            if (isset($_SESSION['reg_lname'])) {
+                echo $_SESSION['reg_lname'];
+            }
+            ?>" required>
+            <br>
 
-        <input type="email" name="reg_email" placeholder="Email" value="<?php 
-        if (isset($_SESSION['reg_email'])) {
-            echo $_SESSION['reg_email'];
-        }
-        ?>" required>
-        <br>
-        <input type="email" name="reg_email2" placeholder="Confirm Email" value="<?php 
-        if (isset($_SESSION['reg_email2'])) {
-            echo $_SESSION['reg_email2'];
-        } 
-        ?>" required>
-        <br>
+            <?php if (in_array(WRONG_LAST_NAME, $error_array)) {
+                echo WRONG_LAST_NAME;
+            } ?>
 
-        <?php if (in_array(EMAIL_ALREADY_IN_USE, $error_array)) { echo EMAIL_ALREADY_IN_USE; } ?>
-        <?php if (in_array(EMAIL_WRONG_FORMAT, $error_array)) { echo EMAIL_WRONG_FORMAT; } ?>
-        <?php if (in_array(EMAILS_NOT_MATCH, $error_array)) { echo EMAILS_NOT_MATCH; } ?>
+            <input type="email" name="reg_email" placeholder="Email" value="<?php 
+            if (isset($_SESSION['reg_email'])) {
+                echo $_SESSION['reg_email'];
+            }
+            ?>" required>
+            <br>
+            <input type="email" name="reg_email2" placeholder="Confirm Email" value="<?php 
+            if (isset($_SESSION['reg_email2'])) {
+                echo $_SESSION['reg_email2'];
+            } 
+            ?>" required>
+            <br>
 
-        <input type="password" name="reg_password" placeholder="Password" value="<?php ?>" required>
-        <br>
-        <input type="password" name="reg_password2" placeholder="Confirm Password" value="<?php ?>" required>
-        <br>
+            <?php if (in_array(EMAIL_ALREADY_IN_USE, $error_array)) { echo EMAIL_ALREADY_IN_USE; } ?>
+            <?php if (in_array(EMAIL_WRONG_FORMAT, $error_array)) { echo EMAIL_WRONG_FORMAT; } ?>
+            <?php if (in_array(EMAILS_NOT_MATCH, $error_array)) { echo EMAILS_NOT_MATCH; } ?>
 
-        <?php if (in_array(PASSWORD_WRONG_FORMAT, $error_array)) { echo PASSWORD_WRONG_FORMAT; } ?>
-        <?php if (in_array(PASSWORD_WRONG_SIZE, $error_array)) { echo PASSWORD_WRONG_SIZE; } ?>
-        <?php if (in_array(PASSWORDS_NOT_MATCH, $error_array)) { echo PASSWORDS_NOT_MATCH; } ?>
+            <input type="password" name="reg_password" placeholder="Password" value="<?php ?>" required>
+            <br>
+            <input type="password" name="reg_password2" placeholder="Confirm Password" value="<?php ?>" required>
+            <br>
 
-        <input type="submit" name="register_button" value="Register">
-        <br>
-        <?php if (in_array(REGISTRATION_OK, $error_array)) { echo REGISTRATION_OK; } ?>
+            <?php if (in_array(PASSWORD_WRONG_FORMAT, $error_array)) { echo PASSWORD_WRONG_FORMAT; } ?>
+            <?php if (in_array(PASSWORD_WRONG_SIZE, $error_array)) { echo PASSWORD_WRONG_SIZE; } ?>
+            <?php if (in_array(PASSWORDS_NOT_MATCH, $error_array)) { echo PASSWORDS_NOT_MATCH; } ?>
 
-    </form>
+            <input type="submit" name="register_button" value="Register">
+            <br>
+            <?php if (in_array(REGISTRATION_OK, $error_array)) { echo REGISTRATION_OK; } ?>
+
+        </form>
     <!-- end Registration form -->
+    </div>
+</div> <!-- end of class wrapper -->
+
+    
 </body>
 </html>
