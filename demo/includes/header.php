8933b52193f1033f935bc6507df43eee39ec5122
roberto <gianottoroberto@gmail.com>
2020-09-10 22:28:05 +0200

    piccolo fix proposto da Sonarlint

--- demo/includes/header.php
+++ demo/includes/header.php
@@ -1,16 +1,18 @@
 <?php
 
+    // check the session like this
     if(session_id() == ''){ 
         session_start();
     }
-
+    // and then
     if (isset($_SESSION["username"])) {
         $userLoggedIn = $_SESSION["username"];
     } else {
         header("Location: register.php");
     }
 ?>
-<html>
+<!DOCTYPE html>
+<html lang="it">
 <head>
     <title>Swirlfeed</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
