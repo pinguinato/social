b915182a01503bfc1d007367dc990bebf82da825
roberto <gianottoroberto@gmail.com>
2020-09-11 06:43:00 +0200

    aggiunta di Bootstrap.js e css

--- demo/includes/header.php
+++ demo/includes/header.php
@@ -16,6 +16,11 @@
 <head>
     <title>Swirlfeed</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="assets/css/bootstrap.css">
+    <script type="text/javascript" src="assets/js/jquery-3.5.1.js"></script>
+    <script type="text/javascript" src="assets/js/bootstrap.js"></script>
+    <script type="text/javascript" src="assets/js/register.js"></script>
+    <link rel="icon" type="ico" href="assets/images/favicon/favicon.ico"/>
 </head>
 <body>
     
