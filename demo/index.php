f4b9aa4e516553e0774fa11bfcbc73ce0f879200
roberto <gianottoroberto@gmail.com>
2020-09-18 19:14:51 +0200

    Creazione della colonna dei newsfeed

--- demo/index.php
+++ demo/index.php
@@ -18,6 +18,15 @@ include("includes/header.php");
             ?>
         </div>
     </div>
+
+    <div class="main_column column">
+        <form class="post_form" action="index.php" method="POST">
+            <textarea name="post_text" id="post_text" placeholder="Got something to say?"></textarea>
+            <input type="submit" name="post" id="post_button" value="Post">
+            <hr>
+        </form>
+    </div>
+
 </div> <!-- end of wrapper div -->
 </body>
 
