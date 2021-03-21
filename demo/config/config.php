c9373c9220479dc475fc363c0c2087c6a91f815d
Roberto Gianotto <gianottoroberto@gmail.com>
2021-03-21 09:07:25 +0100

    resolve merge conflict

--- demo/config/config.php
+++ demo/config/config.php
@@ -1,5 +1,6 @@
-<?php
 
+<?php
+/*
 ob_start(); // Turns on output buffering
 
 session_start();
@@ -18,4 +19,23 @@ $conn = mysqli_connect($host, $user, $password, $database);
 if (mysqli_connect_errno()) {
     echo "Failed to connect " . mysqli_connect_errno();
 }
+*/
 
+ob_start(); // Turns on output buffering
+
+session_start();
+
+$timezone = date_default_timezone_set("Europe/Rome");
+
+// parametri di connessione al mio db locale
+$host = "mysql57";
+$user = "root";
+$password = "root";
+$database ="social";
+
+$conn = mysqli_connect($host, $user, $password, $database);
+
+// connection check
+if (mysqli_connect_errno()) {
+    echo "Failed to connect " . mysqli_connect_errno();
+}
