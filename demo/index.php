89c22aeb22d77bedf63ddbd913f09793b901260c
Roberto Gianotto <gianottoroberto@gmail.com>
2021-03-27 21:17:07 +0100

    impedire al form di fare resubmitting da refresh

--- demo/index.php
+++ demo/index.php
@@ -8,6 +8,7 @@ include("includes/classes/Post.php");
 if (isset($_POST['post'])) {
     $post = new Post($conn, $userLoggedIn);
     $post->submitPost($_POST['post_text'], 'none');
+    header("Location: index.php");
 }
 
 ?>
